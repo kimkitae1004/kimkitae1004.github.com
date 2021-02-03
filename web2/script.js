b75420d638be2574a2c2b1c927a63f692103f386
Kimkitae <31869416+kimkitae1004@users.noreply.github.com>
2021-02-03 15:31:05 +0900

    Add to html document & stylesheet
    html 문서 추가 및 스타일과 스크립트 추가

---
+++ web2/script.js
@@ -0,0 +1,1 @@
+jQuery("#banner5").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:20*100,width:580,height:212,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
